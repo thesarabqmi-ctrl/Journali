1dfcb07743a2266a939b59b21ce031d1a4e49d7a
thesarabqmi-ctrl <thesarabqmi@gmail.com>
2025-10-22 17:41:12 +0300

    Initial Commit

---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by sara on 30/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JournaliTests/JournaliTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliTests.swift
+//  JournaliTests
+//
+//  Created by sara on 30/04/1447 AH.
+//
+
+import Testing
+@testable import Journali
+
+struct JournaliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
